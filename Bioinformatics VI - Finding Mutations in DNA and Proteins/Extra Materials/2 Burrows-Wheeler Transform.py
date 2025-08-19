f7525c0ac303462cac489182de7133f27e5b0ffd
Olivia Rippee <167914008+olivia-rippee@users.noreply.github.com>
2025-08-19 00:52:53 -0500

    Add files via upload

---
+++ Bioinformatics VI - Finding Mutations in DNA and Proteins/Extra Materials/2 Burrows-Wheeler Transform.py
@@ -0,0 +1,8 @@
+import os
+os.chdir("C:/Users/olivi/OneDrive/Python/Bioinformatics VI - Finding Mutations in DNA and Proteins/Data")
+
+# -----------------------------------------------
+# 
+# -----------------------------------------------
+
+
